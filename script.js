b744641c5e6881b8054abc0c0f1244c56bc40883
John Garcia <john.garcia144@gmail.com>
2020-07-17 14:26:56 -0600

    starter code

---
+++ script.js
@@ -0,0 +1,38 @@
+var quizQuestions = [
+    {
+    "quizQuestionHeader" : "Commonly used Data Types do NOT Include:", 
+    "one" : "1. strings",
+    "two" : "2. booleans",
+    "three" : "3. alerts",
+    "four" : "4. numbers",
+    "correct" : "3. alerts",
+    },{
+    "quizQuestionHeader" : "The condition in an if / else statement is enclosed within ________.",
+    "one" : "1. quotes",
+    "two" : "2. curly brackets",
+    "three" : "3. parenthesis",
+    "four" : "4. square brackets",
+    "correct" : "3. parenthesis",
+    },{
+    "quizQuestionHeader" : "Arrays in JavaScript can be used to store ________.",
+    "one" : "1. numbers and strings",
+    "two" : "2. other arrays",
+    "three" : "3. booleans",
+    "four" : "4. all of the above",
+    "correct" : "4. all of the above",
+    },{
+     "quizQuestionHeader" : "String values must be enclosed within ________ when being assigned to variables",
+     "one" : "1. commas",
+     "two" : "2. curly brackets",
+     "three" : "3. quotes",
+     "four" : "4. parenthesis",
+     "correct" : "3. quotes",
+    },{
+     "quizQuestionHeader" : "A very useful tool used for developing and debugging for printing content to the debugger is:",
+     "one" : "1. JavaScript",
+     "two" : "2. terminal / bash",
+     "three" : "3. for loops",
+     "four" : "4. console.log",
+     "correct" : "4. console.log",
+    },
+  ]
